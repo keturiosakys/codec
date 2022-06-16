3d22bb9e41826fbbaf08d6989c6418dc99250701
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2022-06-17 00:37:33 +0200

    query processor if: config, events, or media

--- functions/supabase.js
+++ functions/supabase.js
@@ -18,3 +18,44 @@ async function getMediaAssets() {
     let { data: media_assets, error } = await supabase.from("media_assets").select("*");
     return { media_assets, error };
 }
+
+exports.handler = async (event) => {
+    try {
+        const requested_table = event.queryStringParameters.table;
+
+        if (requested_table === "config") {
+            const { config, error } = await getConfig();
+            // let's try this but remember that config is an array, and we might need an object
+            // in case not:
+            // const finalConfig = config[0];
+            // console.log(config, error);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(config),
+            };
+        } else if (requested_table === "events") {
+            const { events, error } = await getEvents();
+            // console.log(events, error);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(events),
+            };
+        } else if (requested_table === "media_assets") {
+            const { media_assets, error } = await getMediaAssets();
+            // console.log(media_assets, error);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(media_assets),
+            };
+        } else {
+            // console.log("Table not found");
+            return {
+                statusCode: 400,
+                body: "Error: table not found",
+            };
+        }
+    } catch (error) {
+        console.log({ error });
+        return { statusCode: 500, body: error.toString() };
+    }
+};
