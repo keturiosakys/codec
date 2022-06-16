012000416816cd9e73c6715f83f9e6d105e48bdf
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2022-06-17 00:36:36 +0200

    config query

--- functions/supabase.js
+++ functions/supabase.js
@@ -2,3 +2,9 @@ const { createClient } = require("@supabase/supabase-js");
 const { SUPABASE_URL, SUPABASE_KEY } = process.env;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function getConfig() {
+    let { data: config, error } = await supabase.from("config").select("*");
+    // console.log(config, error);
+    return { config, error };
+}
